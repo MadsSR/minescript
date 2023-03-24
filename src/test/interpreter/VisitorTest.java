5e682d3b76d5ce05f8c6a546d28a104634e934e1
Ali Sajad Khorami <33497115+sabotack@users.noreply.github.com>
2023-03-24 13:01:29 +0100

    Test/visitor test (#7)
    * Added gradle to project + implemented several new visitors
    
    * Removed ignored folders
    
    * Added first visitor tests

---
+++ src/test/interpreter/VisitorTest.java
@@ -0,0 +1,64 @@
+package interpreter;
+
+import interpreter.antlr.MineScriptLexer;
+import interpreter.antlr.MineScriptParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+class VisitorTest {
+    private final Visitor visitor = new Visitor();
+
+    @Test
+    void visitAssign() {
+        visitor.visitAssign((MineScriptParser.AssignContext)getStmtTreeFromString("x = 5\n"));
+        Assertions.assertEquals(5, visitor.visitId((MineScriptParser.IdContext)getExprTreeFromString("x")));
+    }
+
+    @Test
+    void visitWhile() {
+    }
+
+    @Test
+    void visitIf() {
+    }
+
+    @Test
+    void visitRepeat() {
+    }
+
+    @Test
+    void visitBool() {
+    }
+
+    @Test
+    void visitComp() {
+    }
+
+    @Test
+    void visitId() {
+    }
+
+    @Test
+    void visitAddSub() {
+        Assertions.assertEquals(10, visitor.visitAddSub((MineScriptParser.AddSubContext)getExprTreeFromString("5 + 5")));
+        Assertions.assertEquals(0, visitor.visitAddSub((MineScriptParser.AddSubContext)getExprTreeFromString("5 - 5")));
+    }
+
+    @Test
+    void visitNumber() {
+    }
+
+    private MineScriptParser.ExpressionContext getExprTreeFromString(String input) {
+        var lexer = new MineScriptLexer(CharStreams.fromString(input));
+        var parser = new MineScriptParser(new CommonTokenStream(lexer));
+        return parser.expression();
+    }
+
+    private MineScriptParser.StatementContext getStmtTreeFromString(String input) {
+        var lexer = new MineScriptLexer(CharStreams.fromString(input));
+        var parser = new MineScriptParser(new CommonTokenStream(lexer));
+        return parser.statement();
+    }
+}
