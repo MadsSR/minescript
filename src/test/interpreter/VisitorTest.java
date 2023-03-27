0bc40971e1f38f3fe95af1e62e3a23f4c63d35cd
Mads Risager <madssr2@gmail.com>
2023-03-27 08:58:18 +0200

    Add tests for vistors; Multiply, Division, Mod, Power, Negative

--- src/test/interpreter/VisitorTest.java
+++ src/test/interpreter/VisitorTest.java
@@ -12,8 +12,8 @@ class VisitorTest {
 
     @Test
     void visitAssign() {
-        visitor.visitAssign((MineScriptParser.AssignContext)getStmtTreeFromString("x = 5\n"));
-        Assertions.assertEquals(5, visitor.visitId((MineScriptParser.IdContext)getExprTreeFromString("x")));
+        visitor.visitAssign((MineScriptParser.AssignContext) getStmtTreeFromString("x = 5\n"));
+        Assertions.assertEquals(5, visitor.visitId((MineScriptParser.IdContext) getExprTreeFromString("x")));
     }
 
     @Test
@@ -42,12 +42,73 @@ class VisitorTest {
 
     @Test
     void visitAddSub() {
-        Assertions.assertEquals(10, visitor.visitAddSub((MineScriptParser.AddSubContext)getExprTreeFromString("5 + 5")));
-        Assertions.assertEquals(0, visitor.visitAddSub((MineScriptParser.AddSubContext)getExprTreeFromString("5 - 5")));
+        Assertions.assertEquals(10, visitor.visitAddSub((MineScriptParser.AddSubContext) getExprTreeFromString("5 + 5")));
+        Assertions.assertEquals(0, visitor.visitAddSub((MineScriptParser.AddSubContext) getExprTreeFromString("5 - 5")));
     }
 
     @Test
     void visitNumber() {
+    }
+
+    @Test
+    void visitMultDivMod() {
+        Assertions.assertEquals(25, visitor.visitMultDivMod((MineScriptParser.MultDivModContext) getExprTreeFromString("5 * 5")));
+        Assertions.assertEquals(1, visitor.visitMultDivMod((MineScriptParser.MultDivModContext) getExprTreeFromString("5 / 5")));
+        Assertions.assertEquals(0, visitor.visitMultDivMod((MineScriptParser.MultDivModContext) getExprTreeFromString("5 % 5")));
+    }
+
+
+    @Test
+    void visitPow() {
+        Assertions.assertEquals(25, visitor.visitPow((MineScriptParser.PowContext) getExprTreeFromString("5 ^ 2")));
+        Assertions.assertEquals(1, visitor.visitPow((MineScriptParser.PowContext) getExprTreeFromString("5 ^ 0")));
+        Assertions.assertEquals(0, visitor.visitPow((MineScriptParser.PowContext) getExprTreeFromString("0 ^ 5")));
+        Assertions.assertEquals(1, visitor.visitPow((MineScriptParser.PowContext) getExprTreeFromString("1 ^ 5")));
+    }
+
+    @Test
+    void visitPowNegativeNumberThrowsException() {
+        Assertions.assertThrows(RuntimeException.class, () -> visitor.visitPow((MineScriptParser.PowContext) getExprTreeFromString("5 ^ -1")));
+    }
+
+//    @Test
+//    void visitPowExpr() {
+//        Assertions.assertEquals(25, visitor.visitPow((MineScriptParser.PowContext) getExprTreeFromString("5 ^ (2 + 0 * 1)")));
+//    }
+
+    @Test
+    void visitPowId() {
+        visitor.visitAssign((MineScriptParser.AssignContext) getStmtTreeFromString("x = 2\n"));
+        Assertions.assertEquals(25, visitor.visitPow((MineScriptParser.PowContext) getExprTreeFromString("5 ^ x")));
+    }
+
+    @Test
+    void visitPowStringThrowsException() {
+        Assertions.assertThrows(RuntimeException.class, () -> visitor.visitPow((MineScriptParser.PowContext) getExprTreeFromString("5 ^ unknown")));
+    }
+
+
+    @Test
+    void visitNegNegativeNumber() {
+        Assertions.assertEquals(-5, visitor.visitNeg((MineScriptParser.NegContext) getExprTreeFromString("-5")));
+        Assertions.assertEquals(-3335, visitor.visitNeg((MineScriptParser.NegContext) getExprTreeFromString("-3335")));
+    }
+
+    @Test
+    void visitNegExpr() {
+        Assertions.assertEquals(-10, visitor.visitNeg((MineScriptParser.NegContext) getExprTreeFromString("-(5 + 5)")));
+        Assertions.assertEquals(-2, visitor.visitNeg((MineScriptParser.NegContext) getExprTreeFromString("-(5 / 5)")));
+    }
+
+    @Test
+    void visitNegId() {
+        visitor.visitAssign((MineScriptParser.AssignContext) getStmtTreeFromString("x = 5\n"));
+        Assertions.assertEquals(-5, visitor.visitNeg((MineScriptParser.NegContext) getExprTreeFromString("-x")));
+    }
+
+    @Test
+    void visitNegStringThrowsException() {
+        Assertions.assertThrows(RuntimeException.class, () -> visitor.visitNeg((MineScriptParser.NegContext) getExprTreeFromString("-unknown")));
     }
 
     private MineScriptParser.ExpressionContext getExprTreeFromString(String input) {
