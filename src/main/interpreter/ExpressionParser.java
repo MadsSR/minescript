b3ef77556bf6758f18d6d0386e70b501ecce0b13
Ali Sajad Khorami <33497115+sabotack@users.noreply.github.com>
2023-03-29 14:40:42 +0200

    All tests now use custom types (#23)

--- src/main/interpreter/ExpressionParser.java
+++ src/main/interpreter/ExpressionParser.java
@@ -2,24 +2,25 @@ package interpreter;
 
 import interpreter.antlr.MineScriptBaseVisitor;
 import interpreter.types.MSBool;
+import interpreter.types.MSNumber;
 
 public class ExpressionParser extends MineScriptBaseVisitor<Object> {
 
     public <T> MSBool getBoolean(T ctx) {
-        if (ctx instanceof Boolean b) {
-            return new MSBool(b);
-        } else if (ctx instanceof Integer i) {
-            return new MSBool(i != 0);
+        if (ctx instanceof MSBool b) {
+            return b;
+        } else if (ctx instanceof MSNumber i) {
+            return new MSBool(i.getValue() != 0);
         } else {
             throw new RuntimeException("Condition must be a bool");
         }
     }
 
     public <T> MSBool getNegatedBoolean(T ctx) {
-        if (ctx instanceof Boolean b) {
-            return new MSBool(!b);
-        } else if (ctx instanceof Integer i) {
-            return new MSBool(i == 0);
+        if (ctx instanceof MSBool b) {
+            return new MSBool(!b.getValue());
+        } else if (ctx instanceof MSNumber i) {
+            return new MSBool(i.getValue() == 0);
         } else {
             throw new RuntimeException("Condition must be a bool");
         }
