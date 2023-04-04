c190f98471a2d8602541b1e258e6c3b98472f891
sabotack <sabotack.2001@gmail.com>
2023-04-04 09:50:22 +0200

    WIP block movement

--- src/main/java/minescript/block/entity/TurtleBlockEntity.java
+++ src/main/java/minescript/block/entity/TurtleBlockEntity.java
@@ -1,13 +1,16 @@
 package minescript.block.entity;
 
 import minescript.item.inventory.ImplementedInventory;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.state.property.Properties;
 import net.minecraft.text.Text;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
@@ -15,6 +18,7 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class TurtleBlockEntity extends BlockEntity implements NamedScreenHandlerFactory, ImplementedInventory {
+
     public TurtleBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.TURTLE_BLOCK_ENTITY, pos, state);
     }
@@ -37,5 +41,22 @@ public class TurtleBlockEntity extends BlockEntity implements NamedScreenHandler
 
     public static void tick(World world, BlockPos pos, BlockState state, TurtleBlockEntity entity) {
 
+    }
+
+    public void step(int steps) {
+        if (!world.isClient()) {
+            BlockState state = world.getBlockState(pos);
+            BlockPos newPos = pos;
+
+            switch (state.get(Properties.HORIZONTAL_FACING)) {
+                case NORTH -> newPos = newPos.north(steps);
+                case SOUTH -> newPos = newPos.south(steps);
+                case EAST -> newPos = newPos.east(steps);
+                case WEST -> newPos = newPos.west(steps);
+            }
+
+            world.setBlockState(newPos, state, Block.NOTIFY_ALL);
+            world.setBlockState(pos, Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL);
+        }
     }
 }
--- src/main/java/minescript/screen/TextEditorScreen.java
+++ src/main/java/minescript/screen/TextEditorScreen.java
@@ -1,24 +1,39 @@
 package minescript.screen;
 
 import io.wispforest.owo.ui.base.BaseUIModelScreen;
+import io.wispforest.owo.ui.component.ButtonComponent;
 import io.wispforest.owo.ui.container.FlowLayout;
+import minescript.block.entity.TurtleBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.gui.widget.EditBoxWidget;
-import net.minecraft.util.Identifier;
 
 public class TextEditorScreen extends BaseUIModelScreen<FlowLayout> {
-    public TextEditorScreen() {
+
+    private BlockEntity blockEntity;
+    public TextEditorScreen(BlockEntity blockEntity) {
         super(FlowLayout.class, DataSource.file("../src/main/resources/assets/minescript/owo_ui/editor.xml"));
         //super(FlowLayout.class, DataSource.asset(new Identifier("minescript", "editor")));
+        this.blockEntity = blockEntity;
     }
 
     @Override
     protected void build(FlowLayout rootComponent) {
         var editor = rootComponent.childById(EditBoxWidget.class, "edit-box");
-        editor.active = true;
 
+        if (editor == null) throw new NullPointerException("editor is null");
+        editor.active = true;
         editor.mouseDown().subscribe((a,b,c) -> {
             editor.setFocused(true);
+
             return true;
+        });
+
+        var runButton = rootComponent.childById(ButtonComponent.class, "run-button");
+        if (runButton == null) throw new NullPointerException("runButton is null");
+        runButton.onPress(button -> {
+            if (blockEntity instanceof TurtleBlockEntity turtleBlockEntity) {
+                turtleBlockEntity.step(Integer.parseInt(editor.getText()));
+            }
         });
     }
 }
