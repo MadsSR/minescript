f40655a3fff78b3857e6cb65baf8985cb4600943
Mads Risager <madssr2@gmail.com>
2023-05-02 13:32:52 +0200

    Fix build settings

--- src/main/java/minescript/screen/TextEditorScreen.java
+++ src/main/java/minescript/screen/TextEditorScreen.java
@@ -8,13 +8,14 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.widget.EditBoxWidget;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 public class TextEditorScreen extends BaseUIModelScreen<FlowLayout> {
     private TurtleBlockEntity turtleBlockEntity;
 
     public TextEditorScreen(BlockEntity blockEntity) {
-        super(FlowLayout.class, DataSource.file("../src/main/resources/assets/minescript/owo_ui/editor.xml"));
-        //super(FlowLayout.class, DataSource.asset(new Identifier("minescript", "editor")));
+//        super(FlowLayout.class, DataSource.file("../src/main/resources/assets/minescript/owo_ui/editor.xml"));
+        super(FlowLayout.class, DataSource.asset(new Identifier("minescript", "editor")));
 
         if (blockEntity instanceof TurtleBlockEntity turtle) {
             turtleBlockEntity = (TurtleBlockEntity) blockEntity;
