3562334a31a92d09e9fb892600cb996fac4bcb29
sabotack <sabotack.2001@gmail.com>
2023-05-11 09:18:20 +0200

    Added visitId tests

--- src/test/interpreter/VisitorUnitTest.java
+++ src/test/interpreter/VisitorUnitTest.java
@@ -3,32 +3,31 @@ package interpreter;
 import interpreter.types.*;
 import interpreter.utils.MockTerminalNode;
 import interpreter.antlr.MineScriptParser;
+import net.minecraft.block.*;
+import net.minecraft.util.Identifier;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.concurrent.atomic.AtomicReference;
-
-import static interpreter.utils.TreeFromString.getExprTreeFromString;
 
 @ExtendWith(MockitoExtension.class)
 class VisitorUnitTest {
     private final SymbolTable symbolTable = new SymbolTable();
     private final Visitor visitor = new Visitor(symbolTable);
 
-    @Spy Visitor spyVisitor = visitor;
-    @Mock MineScriptParser.AssignContext mockAssignContext;
-    @Mock MineScriptParser.ExpressionContext mockExpressionContext;
-    @Mock MineScriptParser.BoolContext mockBoolContext;
-    @Mock MineScriptParser.AbsDirContext mockAbsDirContext;
-    @Mock MineScriptParser.RelDirContext mockRelDirContext;
-    @Mock MineScriptParser.NumberContext mockNumberContext;
+    @Spy private final Visitor spyVisitor = visitor;
+    @Mock private MineScriptParser.AssignContext mockAssignContext;
+    @Mock private MineScriptParser.ExpressionContext mockExpressionContext;
+    @Mock private MineScriptParser.BoolContext mockBoolContext;
+    @Mock private MineScriptParser.AbsDirContext mockAbsDirContext;
+    @Mock private MineScriptParser.RelDirContext mockRelDirContext;
+    @Mock private MineScriptParser.NumberContext mockNumberContext;
+    @Mock private MineScriptParser.IdContext mockIdContext;
 
     @ParameterizedTest
     @ValueSource(ints = {-1000, -10, 0, 10, 1000})
@@ -50,48 +49,76 @@ class VisitorUnitTest {
         Assertions.assertNull(result);
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {-1000, -10, 0, 10, 1000})
+    void visitIdValidIdReturnsValue(int value) {
+        Mockito.when(mockIdContext.ID()).thenReturn(new MockTerminalNode("varName"));
+        symbolTable.enterSymbol("varName", new MSNumber(value));
+
+        MSType result = spyVisitor.visitId(mockIdContext);
+        Assertions.assertEquals(((MSNumber) result).getValue(), value);
+    }
+
+    @Test
+    void visitIdInvalidIdThrowsRuntimeException() {
+        Mockito.when(mockIdContext.ID()).thenReturn(new MockTerminalNode("varName"));
+        Assertions.assertThrows(RuntimeException.class, () -> spyVisitor.visitId(mockIdContext));
+    }
+
     @Test
     void visitBoolPassTrueReturnsTrue() {
         Mockito.when(mockBoolContext.getText()).thenReturn("true");
-        Assertions.assertTrue(((MSBool) visitor.visitBool(mockBoolContext)).getValue());
+        Assertions.assertTrue(((MSBool) spyVisitor.visitBool(mockBoolContext)).getValue());
     }
 
     @Test
     void visitBoolPassFalseReturnsFalse() {
         Mockito.when(mockBoolContext.getText()).thenReturn("false");
-        Assertions.assertFalse(((MSBool) visitor.visitBool(mockBoolContext)).getValue());
+        Assertions.assertFalse(((MSBool) spyVisitor.visitBool(mockBoolContext)).getValue());
     }
 
     @Test
     void visitBoolPassRandomStringReturnsFalse() {
         Mockito.when(mockBoolContext.getText()).thenReturn("abc");
-        Assertions.assertFalse(((MSBool) visitor.visitBool(mockBoolContext)).getValue());
+        Assertions.assertFalse(((MSBool) spyVisitor.visitBool(mockBoolContext)).getValue());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"north", "south", "east", "west", "top", "bottom"})
     void visitAbsDirValidInputReturnsAbsDir(String value){
         Mockito.when(mockAbsDirContext.ABSDIR()).thenReturn(new MockTerminalNode(value));
-        Assertions.assertEquals(((MSAbsDir) visitor.visitAbsDir(mockAbsDirContext)).getValue(), new MSAbsDir(value).getValue());
+        Assertions.assertEquals(((MSAbsDir) spyVisitor.visitAbsDir(mockAbsDirContext)).getValue(), new MSAbsDir(value).getValue());
+    }
+
+    @Test
+    void visitAbsDirInvalidInputThrowsIllegalArgumentException(){
+        Mockito.when(mockAbsDirContext.ABSDIR()).thenReturn(new MockTerminalNode("abc"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> spyVisitor.visitAbsDir(mockAbsDirContext));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"up", "down", "left", "right"})
     void visitRelDirValidInputReturnsRelDir(String value) {
         Mockito.when(mockRelDirContext.RELDIR()).thenReturn(new MockTerminalNode(value));
-        Assertions.assertEquals(((MSRelDir) visitor.visitRelDir(mockRelDirContext)).getValue(), new MSRelDir(value).getValue());
+        Assertions.assertEquals(((MSRelDir) spyVisitor.visitRelDir(mockRelDirContext)).getValue(), new MSRelDir(value).getValue());
+    }
+
+    @Test
+    void visitRelDirInvalidInputThrowsIllegalArgumentException(){
+        Mockito.when(mockRelDirContext.RELDIR()).thenReturn(new MockTerminalNode("abc"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> spyVisitor.visitRelDir(mockRelDirContext));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-1000, -10, 0, 10, 1000})
     void visitNumberValidInputReturnsNumber(int value) {
         Mockito.when(mockNumberContext.NUMBER()).thenReturn(new MockTerminalNode(String.valueOf(value)));
-        Assertions.assertEquals(((MSNumber) visitor.visitNumber(mockNumberContext)).getValue(), new MSNumber(value).getValue());
+        Assertions.assertEquals(((MSNumber) spyVisitor.visitNumber(mockNumberContext)).getValue(), new MSNumber(value).getValue());
     }
 
     @Test
     void visitNumberInvalidInputThrowsIllegalArgumentException(){
         Mockito.when(mockNumberContext.NUMBER()).thenReturn(new MockTerminalNode("abc"));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> visitor.visitNumber(mockNumberContext));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> spyVisitor.visitNumber(mockNumberContext));
     }
 }
