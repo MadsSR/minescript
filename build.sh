92d041b79a10427044429af4ea74b1d391b366c9
sabotack <sabotack.2001@gmail.com>
2023-05-12 12:04:53 +0200

    Removed unnecessary spacing

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ fi
 cd src/main/interpreter/antlr || exit 1
 antlr4 -visitor -no-listener MineScript.g4 || exit 1
 
-sed -i '1s/^/package interpreter.antlr;\n\n /' MineScript*.java || exit 1
+sed -i '1s/^/package interpreter.antlr;\n\n/' MineScript*.java || exit 1
