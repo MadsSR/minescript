85eb90a744e967eae844f640a9fdce3a736b2763
sabotack <sabotack.2001@gmail.com>
2023-05-12 12:00:45 +0200

    Updated build script to build antlr files

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-javac src/main/interpreter/antlr/MineScript*.java src/main/interpreter/Interpreter.java
+#!/bin/bash
+
+if [[ $(pwd) != *"/MineScript" ]]; then
+  echo "This script should be executed within the 'MineScript' directory."
+  exit 1
+fi
+
+cd src/main/interpreter/antlr || exit 1
+antlr4 -visitor -no-listener MineScript.g4 || exit 1
+
+sed -i '1s/^/package interpreter.antlr;\n\n /' MineScript*.java || exit 1
